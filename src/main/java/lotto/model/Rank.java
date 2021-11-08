188d5c6c438d969bc0716867e5524236edc914e7
이승민 <sml7179@naver.com>
2021-11-09 00:49:59 +0900

    fix: 랭크가 순서대로 안나오는 버그 수정

--- src/main/java/lotto/model/Rank.java
+++ src/main/java/lotto/model/Rank.java
@@ -32,7 +32,7 @@ public enum Rank {
 
     public static List<Rank> createWinningRanks() {
         return Stream.of(FIRST, SECOND, THIRD, FOURTH, FIFTH)
-                .sorted(Comparator.comparing(Rank::getCountOfMatch))
+                .sorted(Comparator.comparing(Rank::getWinningMoney))
                 .collect(Collectors.toList());
     }
 
